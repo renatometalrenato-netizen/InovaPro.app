9b1eb8a4a2686d297851e2d45dd312537bbca60c
renatometalrenato-netizen <renato.metal.renato@gmail.com>
2026-09-19 00:23:08 -0300

    Update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,1 +1,9 @@
-supabase.ts
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://tygksrzhqqjsltmycvxp.supabase.co'
+const supabasePublishableKey = 'sb_publishable_8LFlbAZtz6pvbChWFWR8zw_PX4HrIdp'
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabasePublishableKey
+)
