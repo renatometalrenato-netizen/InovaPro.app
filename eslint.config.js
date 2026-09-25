1217fba96e23a7a469a6737139c327e9832010b5
renatometalrenato-netizen <renato.metal.renato@gmail.com>
2026-09-25 06:29:00 -0300

    chore: keep Deno edge functions outside browser eslint scope

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'supabase/functions/**'] },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   reactHooks.configs.flat.recommended,
